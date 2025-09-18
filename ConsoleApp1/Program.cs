3e17b30352fb7cd77506fe9fbd4039a97490d197
arghavansoftwareco <amir.h7800@gmail.com>
2025-09-18 19:35:37 +0330

    l

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,14 +3,14 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello, World! 2");
 
-
-void Tst1)
-{
-
-    string n1ame2 = "ali 33";
-}
 void T1es2t2501_07022223500()
 { 
 
     string n1ame2 = "ali 33";
+}
+
+void Test1()
+{
+
+    string n1ame2 = "s";
 }
